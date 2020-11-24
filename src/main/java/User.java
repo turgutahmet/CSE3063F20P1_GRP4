e3110f875a8a67c3e13600ba95b98529be59534d
Fatih <32433330+fathbas@users.noreply.github.com>
2020-11-24 22:55:49 +0300

    User system has been modeled.

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+public class User {
+    //Define basic User variable
+    private UserInfo[] userInfos;
+
+    //Variables getter setter methods
+    public UserInfo[] getUsers() {
+        return userInfos;
+    }
+    public void setUsers(UserInfo[] userInfos) {
+        this.userInfos = userInfos;
+    }
+}
---
+++ src/main/java/UserInfo.java
@@ -0,0 +1,28 @@
+public class UserInfo {
+    //Define basic UserInfo variable
+    private int userID;
+    private String userName;
+    private String userType;
+
+    //Variables getter setter methods
+    public int getUserID() {
+        return userID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
