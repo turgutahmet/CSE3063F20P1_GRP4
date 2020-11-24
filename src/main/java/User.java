60fa92bd4118fbff921aff91855390e1598f9961
Ahmet TURGUT <turgutahmt@gmail.com>
2020-11-24 23:33:55 +0300

    User system has been read.

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,11 +1,16 @@
+import com.fasterxml.jackson.annotation.*;
+
 public class User {
     //Define basic User variable
     private UserInfo[] userInfos;
+
+    //Json property: The feature in which variables in json file which variables we should assign in our model.
 
     //Variables getter setter methods
     public UserInfo[] getUsers() {
         return userInfos;
     }
+    @JsonProperty("users")
     public void setUsers(UserInfo[] userInfos) {
         this.userInfos = userInfos;
     }
--- src/main/java/UserInfo.java
+++ src/main/java/UserInfo.java
@@ -1,13 +1,18 @@
+import com.fasterxml.jackson.annotation.*;
+
 public class UserInfo {
     //Define basic UserInfo variable
     private int userID;
     private String userName;
     private String userType;
 
+    //Json property: The feature in which variables in json file which variables we should assign in our model.
+
     //Variables getter setter methods
     public int getUserID() {
         return userID;
     }
+    @JsonProperty("user id")
     public void setUserID(int userID) {
         this.userID = userID;
     }
@@ -15,6 +20,7 @@ public class UserInfo {
     public String getUserName() {
         return userName;
     }
+    @JsonProperty("user name")
     public void setUserName(String userName) {
         this.userName = userName;
     }
@@ -22,6 +28,7 @@ public class UserInfo {
     public String getUserType() {
         return userType;
     }
+    @JsonProperty("user type")
     public void setUserType(String userType) {
         this.userType = userType;
     }
