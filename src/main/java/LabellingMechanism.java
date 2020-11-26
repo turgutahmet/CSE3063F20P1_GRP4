d3b5f9a25bcf11320e1fa072202fd8a7061e5e27
KorayEmreSenel <57563704+KorayEmreSenel@users.noreply.github.com>
2020-11-26 22:30:44 +0300

    Add files via upload

---
+++ src/main/java/LabellingMechanism.java
@@ -0,0 +1,9 @@
+public abstract class LabellingMechanism {
+    public String labellingMechanismType;
+
+    public ClassLabel labelInstanceWithUser(User user, Instance instance, ClassLabel[] classLabel) {
+        //returns class label according labelling mechanism type
+        return null;
+    }
+    
+}
