12d9482c1960f1ea04057a65085d20338f3c744a
Etka Uzun <50289806+etka98@users.noreply.github.com>
2020-11-26 22:44:32 +0300

    Add files via upload
    Create RandomLabelling Class

---
+++ src/main/java/RandomLabelling.java
@@ -0,0 +1,6 @@
+public class RandomLabelling extends LabellingMechanism{
+    @Override
+    public ClassLabel labelInstanceWithUser(User user, Instance instance, ClassLabel[] classLabel) {
+        return null;
+    }
+}
