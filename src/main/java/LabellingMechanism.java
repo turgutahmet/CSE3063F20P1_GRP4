3104495d654d5b06a4b7b6a249927f7e1973e694
KorayEmreSenel <57563704+KorayEmreSenel@users.noreply.github.com>
2020-11-28 23:12:05 +0300

    Created the LabellingMechanism class

---
+++ src/main/java/LabellingMechanism.java
@@ -0,0 +1,14 @@
+public abstract class LabellingMechanism {
+
+    private String labellingMechanismType;
+
+    public abstract ClassLabel labelInstanceWithUser(User user, Instance instance, ClassLabel[] classLabel);
+
+    public String getLabellingMechanismType() {
+        return labellingMechanismType;
+    }
+
+    public void setLabellingMechanismType(String labellingMechanismType) {
+        this.labellingMechanismType = labellingMechanismType;
+    }
+}
