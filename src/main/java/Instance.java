cd832a1845dcc283bc16959d1307cd750378fc11
alperenkoker <alperen.koker97@gmail.com>
2020-11-30 16:27:27 +0300

    addLabel method in Instance class has implemented.

--- src/main/java/Instance.java
+++ src/main/java/Instance.java
@@ -1,9 +1,22 @@
 import com.fasterxml.jackson.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Instance {
     //Define basic Instance variable
     private int id;
     private String instance;
+    private boolean hasLabeled = false;
+    private ArrayList<LabelledInstance> labelPairs = new ArrayList<LabelledInstance>();
+
+    public void addLabel(UserInfo userInfo,ClassLabel classLabel){
+        if(hasLabeled==false){
+            hasLabeled=true;
+        }
+        LabelledInstance labelledInstance = new LabelledInstance(userInfo,classLabel, LocalDate.now());
+        labelPairs.add(labelledInstance);
+    }
     //Json property: The feature in which variables in json file which variables we should assign in our model.
 
     //Variables getter setter methods
@@ -11,7 +24,25 @@ public class Instance {
     @JsonProperty("id")
     public void setID(int value) { this.id = value; }
 
-    public String getInstance() { return instance; }
+    public String getInstance() {
+
+        return instance; }
     @JsonProperty("instance")
     public void setInstance(String value) { this.instance = value; }
+
+    public boolean isHasLabeled() {
+        return hasLabeled;
+    }
+
+    public void setHasLabeled(boolean hasLabeled) {
+        this.hasLabeled = hasLabeled;
+    }
+
+    public ArrayList<LabelledInstance> getLabelPairs() {
+        return labelPairs;
+    }
+
+    public void setLabelPairs(ArrayList<LabelledInstance> labelPairs) {
+        this.labelPairs = labelPairs;
+    }
 }
