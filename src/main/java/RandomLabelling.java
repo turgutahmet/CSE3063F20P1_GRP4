ec24b84bdef3ee9bba34f9fa9e24620de0821906
Etka Uzun <50289806+etka98@users.noreply.github.com>
2020-11-30 17:04:51 +0300

    Update the parameter of labelInstanceWithUser
    changing Array of classLabel to ArrayList classLabel

--- src/main/java/RandomLabelling.java
+++ src/main/java/RandomLabelling.java
@@ -1,9 +1,9 @@
 import java.lang.Math;
+import java.util.*;
 public class RandomLabelling extends LabellingMechanism{
     @Override
-    public ClassLabel labelInstanceWithUser( UserInfo userInfo, Instance instance, ClassLabel[] classLabel) {
-        int random_index= (int)(Math.random() * classLabel.length) ;
-        return classLabel[random_index] ;
-     
+    public ClassLabel labelInstanceWithUser( UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabel) {
+        int random_index= (int)(Math.random() * classLabel.size()) ;
+        return classLabel.get(random_index);
     }
 }
