f50bdc4f1ff408ba8353815285d0682a64ee6dfe
koray <koray.emresenel@hotmail.com>
2020-12-04 23:05:48 +0300

    Updated the labelInstanceWithUser method. #13

--- src/main/java/RandomLabelling.java
+++ src/main/java/RandomLabelling.java
@@ -1,7 +1,26 @@
 import java.lang.Math;
 import java.util.*;
-public class RandomLabelling extends LabellingMechanism{
+public class RandomLabelling extends LabellingMechanism {
     @Override
-    public void labelInstanceWithUser( UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabel) {
+    public void labelInstanceWithUser(UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabels) {
+
+        int howManyLabels = (int) (Math.random() * classLabels.size()) + 1;
+        ArrayList<ClassLabel> labelArray = new ArrayList<>();
+        ArrayList<ClassLabel> availableLabels = new ArrayList<>();
+        for (ClassLabel label : classLabels) {
+            availableLabels.add(label);
+        }
+
+        for (int i = 0; i < howManyLabels; i++) {
+
+            int random_index = (int) (Math.random() * availableLabels.size());
+
+            labelArray.add(availableLabels.get(random_index));
+
+            availableLabels.remove(availableLabels.get(random_index));
+
+        }
+
+        instance.addLabel(userInfo, labelArray);
     }
 }
