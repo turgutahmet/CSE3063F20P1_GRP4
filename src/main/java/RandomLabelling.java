915793172299caa23f9524b5c39f1b0591ab0b39
koray <koray.emresenel@hotmail.com>
2020-12-05 02:18:41 +0300

    Max amount of labels for instances are getting checked.

--- src/main/java/RandomLabelling.java
+++ src/main/java/RandomLabelling.java
@@ -6,31 +6,37 @@ public class RandomLabelling extends LabellingMechanism {
     @Override
     public void labelInstanceWithUser(UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabels, Logger logger) {
 
-        int howManyLabels = (int) (Math.random() * classLabels.size()) + 1;
+        int howManyLabels = (int) (Math.random() * instance.getMaxNumberOfLabel()) + 1;
 
-        if(howManyLabels > instance.getAmountOfLabels())
-            howManyLabels = (int) (Math.random() * instance.getMaxNumberOfLabel()) + 1;
-
+        int filledLabes = 0;
         ArrayList<ClassLabel> labelArray = new ArrayList<>();
         ArrayList<ClassLabel> availableLabels = new ArrayList<>();
         for (ClassLabel label : classLabels) {
             availableLabels.add(label);
         }
 
+        for (int i = 0 ; i<instance.getLabelPairs().size(); i++) {
+            filledLabes += instance.getLabelPairs().get(i).getLabels().size();
+        }
+
         for (int i = 0; i < howManyLabels; i++) {
+            if(filledLabes == instance.getMaxNumberOfLabel())
+                if(i == 0)
+                    return;
+                else
+                    break;
 
             int random_index = (int) (Math.random() * availableLabels.size());
 
             labelArray.add(availableLabels.get(random_index));
 
             availableLabels.remove(availableLabels.get(random_index));
+            filledLabes++;
 
         }
-        //TODO: copy instance object and check amount of labels
-        //TODO: if amount of labels is less than max number of labels save changes
-        //TODO: if amount of labels is greater than max number of labels rollback labeling process and re-calculate howManyLabels
-        //TODO: if amount of labels is equal to max number of labels save changes
+
         Collections.sort(labelArray , Comparator.comparing(ClassLabel::getLabelID));
         instance.addLabel(userInfo, labelArray,logger);
+        System.out.println(filledLabes);
     }
 }
