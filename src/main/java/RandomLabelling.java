ad7dd4b1b36554f09183521abab4a30fd55f9e2f
Etka Uzun <50289806+etka98@users.noreply.github.com>
2020-12-04 22:48:52 +0300

    change labelInstanceWithUser method return type
    change labelInstanceWithUser method return type Instance to Void #13

--- src/main/java/RandomLabelling.java
+++ src/main/java/RandomLabelling.java
@@ -2,9 +2,6 @@ import java.lang.Math;
 import java.util.*;
 public class RandomLabelling extends LabellingMechanism{
     @Override
-    public Instance labelInstanceWithUser( UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabel) {
-        int random_index= (int)(Math.random() * classLabel.size()) ;
-        Instance labelledInstance = instance.addLabel(userInfo, classLabel.get(random_index));
-        return labelledInstance;
+    public void labelInstanceWithUser( UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabel) {
     }
 }
