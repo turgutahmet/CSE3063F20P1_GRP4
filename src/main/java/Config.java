4e1e27689c595c72ec57ce6a2311ecd66216e23f
Ahmet TURGUT <turgutahmt@gmail.com>
2020-12-16 19:02:07 +0300

    User class has been renamed to Config class && New attributes have been added in Config class

---
+++ src/main/java/Config.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.ArrayList;
+
+public class Config {
+    //Define basic User variable
+    private int currentDatasetID;
+    private ArrayList<UserInfo> userInfos;
+    private ArrayList<DatasetInfo> datasetInfos;
+
+    //Json property: The feature in which variables in json file which variables we should assign in our model.
+
+    //Variables getter setter methods
+    public ArrayList<UserInfo> getUserInfos() {
+        return userInfos;
+    }
+    @JsonProperty("users")
+    public void setUserInfos(ArrayList<UserInfo> userInfos) {
+        this.userInfos = userInfos;
+    }
+
+    public int getCurrentDatasetID() {
+        return currentDatasetID;
+    }
+    @JsonProperty("current dataset id")
+    public void setCurrentDatasetID(int currentDatasetID) {
+        this.currentDatasetID = currentDatasetID;
+    }
+
+    public ArrayList<DatasetInfo> getDatasetInfos() {
+        return datasetInfos;
+    }
+    @JsonProperty("datasets")
+    public void setDatasetInfos(ArrayList<DatasetInfo> datasetInfos) {
+        this.datasetInfos = datasetInfos;
+    }
+}
