e1215b7312d0e394d5518c0bc3de3f15c87abbc0
koray <koray.emresenel@hotmail.com>
2020-12-22 22:23:09 +0300

    Created UserAndPercentage class

---
+++ src/main/java/UserAndPercentage.java
@@ -0,0 +1,16 @@
+public class UserAndPercentage {
+    private String userName;
+    private float percentage;
+
+    public UserAndPercentage(String username, float percentage) {
+        this.userName = username;
+        this.percentage = percentage;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public float getPercentage() {
+        return percentage;
+    }
+}
