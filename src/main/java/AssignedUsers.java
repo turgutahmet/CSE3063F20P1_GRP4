9a4d854893b03f11b94e71e07bd880a8a784d532
Ahmet TURGUT <turgutahmt@gmail.com>
2020-12-24 14:37:54 +0300

    Has been added AssignedUsers class.

---
+++ src/main/java/AssignedUsers.java
@@ -0,0 +1,38 @@
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+
+public class AssignedUsers {
+    private Config config;
+    private DatasetInfo currentDatasetInfo;
+    private Logger logger;
+    private ArrayList<UserInfo> allUsers;
+    private ArrayList<UserInfo> currentUser;
+
+    public AssignedUsers(Config config, DatasetInfo currentDatasetInfo, Logger logger) {
+        this.config = config;
+        this.currentDatasetInfo = currentDatasetInfo;
+        this.logger = logger;
+    }
+
+    public ArrayList<UserInfo> getAllUsers() {
+        return allUsers;
+    }
+
+    public ArrayList<UserInfo> getCurrentUsers() {
+        return currentUser;
+    }
+
+    public AssignedUsers invoke() {
+        allUsers = config.getUserInfos();
+        currentUser = new ArrayList<>();
+        for (UserInfo userInfo : allUsers) {
+            //Print config log records check log.txt.
+            if (currentDatasetInfo.getAssignUserID().contains(userInfo.getUserID())) {
+                logger.info("config: created " + userInfo.getUsername() + " as " + userInfo.getUserType());
+                currentUser.add(userInfo);
+            }
+        }
+        return this;
+    }
+}
