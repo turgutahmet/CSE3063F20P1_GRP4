9d7829a35cdc018ef0415dda2f8df56f3b9da6af
Ahmet TURGUT <turgutahmt@gmail.com>
2020-12-24 14:54:27 +0300

    Has been added invoke method.

--- src/main/java/UserPerformanceMetricsCreator.java
+++ src/main/java/UserPerformanceMetricsCreator.java
@@ -9,5 +9,31 @@ public class UserPerformanceMetricsCreator {
         this.datasetsInfo = datasetsInfo;
         this.currentUsers = currentUsers;
     }
+    public void invoke() {
+        //Create and set all user performance metrics.
+        for (UserInfo userInfo : currentUsers) {
+            //Create a a File object related to that user's UserPerformanceMetrics.
+            UserPerformanceMetrics userPerformanceMetrics;
+            File dir = new File("./database/");
+            File file = new File("./database/User"+userInfo.getUserID() + ".json");
 
+            //If that File object exists read from that file and set user performance metrics.
+            if (file.exists()) {
+                userPerformanceMetrics = (UserPerformanceMetrics) FileReader.getReader().read(new File(dir, file.getName()), new UserPerformanceMetrics());
+            } else { //If it isn't exist create new one.
+                int numberOfAssignedDataset = 0;
+                for (DatasetInfo datasetInfo : datasetsInfo) {
+                    for (Integer integer : datasetInfo.getAssignUserID()) {
+                        if (userInfo.getUserID() == integer)
+                            numberOfAssignedDataset++;
+                    }
+
+                }
+                userPerformanceMetrics = new UserPerformanceMetrics(userInfo, numberOfAssignedDataset);
+            }
+
+            //Set user's user performance metrics.
+            userInfo.setUserPerformanceMetrics(userPerformanceMetrics);
+        }
+    }
 }
