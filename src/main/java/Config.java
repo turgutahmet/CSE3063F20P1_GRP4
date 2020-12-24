ffed9d53a73927c2beb5edeb76a9e5b246b62f27
etka <etomehmet1907@gmail.com>
2020-12-24 15:02:30 +0300

    Config class has been updated

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.annotation.*;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Config {
@@ -7,6 +8,10 @@ public class Config {
     private int currentDatasetID;
     private ArrayList<UserInfo> userInfos;
     private ArrayList<DatasetInfo> datasetInfos;
+
+    public Config readConfig() {
+        return (Config) FileReader.getReader().read(new File("src/config.json"), this);
+    }
 
     //Json property: The feature in which variables in json file which variables we should assign in our model.
 
