0ef0df7066a6c8be704d24bc54500facf3c14695
koray <koray.emresenel@hotmail.com>
2021-01-02 04:38:18 +0300

    Added the labelInstanceWithUser method in UserLabeling class.

--- src/main/java/UserLabeling.java
+++ src/main/java/UserLabeling.java
@@ -11,6 +11,22 @@ public class UserLabeling extends LabelingMechanism {
     public void labelInstanceWithUser(UserInfo userInfo, Instance instance, ArrayList<ClassLabel> classLabels) {
        }
 
+       public void labelInstanceWithUser(UserInfo userInfo, Instance instance ,ClassLabel label){
+
+        LabelCounter labelCounter = new LabelCounter(label);
+
+        LabeledInstance labeledInstance = instance.createLabeledInstance(userInfo);
+
+        instance.getInstancePerformanceMetrics().addNewLabelAssignment(userInfo, label);
+
+        instance.updateInstance(labeledInstance, labelCounter);
+
+        Logger logger = Logger.getLogger(this.getClass().getName());
+
+        logger.info("user id:" + userInfo.getUserID() + " " + userInfo.getUsername() + " " + "labeled instance id:" + instance.getID() + " "
+                + "with class label " + labelCounter.getLabel().getLabelID() + " :" + labelCounter.getLabel().getLabelText() + " " + "instance :" + instance.getInstance());
+
+    }
 
 
 }
