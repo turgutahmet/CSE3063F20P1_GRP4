9715c71a4646195380930dee59a089d962b72770
Fatih <32433330+fathbas@users.noreply.github.com>
2021-01-09 13:12:40 +0300

    Config json file and Config class have been created.

---
+++ Config.py
@@ -0,0 +1,9 @@
+import json
+
+
+class Config:
+
+    def __init__(self):
+        self.pollResultDirectory = data["poll result directory"]
+        self.answerKeyDirectory = data["answer key directory"]
+        self.studentListDirectory = data["student list directory"]
