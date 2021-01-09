c6f60ef4aaeb68256c4a3207a36863f74147696d
Ahmet TURGUT <turgutahmt@gmail.com>
2021-01-09 13:19:06 +0300

    Config file has been read.

--- Config.py
+++ Config.py
@@ -4,6 +4,13 @@ import json
 class Config:
 
     def __init__(self):
+        data = self.readJson()
         self.pollResultDirectory = data["poll result directory"]
         self.answerKeyDirectory = data["answer key directory"]
         self.studentListDirectory = data["student list directory"]
+
+    @staticmethod
+        def readJson():
+            with open('config.json') as data:
+                data = json.load(data)
+                return data
