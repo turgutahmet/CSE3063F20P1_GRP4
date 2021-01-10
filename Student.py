f0aab6f1bb53d7ac8d15e9212bb5025508816dbb
etka <etomehmet1907@gmail.com>
2021-01-10 20:17:08 +0300

    Create Student.py

---
+++ Student.py
@@ -0,0 +1,5 @@
+class Student:
+    def __init__(self,studentNumber,FirstName,lastName):
+        self.studentNumber = studentNumber
+        self.firstName = FirstName
+        self.lastName = lastName
