157e353032ea27ee3223fa7529d92dd263a2dace
esraa turky <42881629+esraa-torky@users.noreply.github.com>
2021-01-10 20:24:03 +0300

    PollAnalyzer class created

---
+++ PollAnalyzer.py
@@ -0,0 +1,18 @@
+from Question import *
+from Config import *
+from Student import *
+from Poll import *
+import xlrd as xlrd
+class PollAnalyzer():
+    def __init__(self):
+        self.students=[]
+        self.questions=[]
+        self.polls=[]
+        self.pollResults=[]
+        self.config= Config()
+
+    
+
+
+
+
