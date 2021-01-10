d839659f4c860bd406f4e604309cdb1db218675f
koray <koray.emresenel@hotmail.com>
2021-01-10 20:18:56 +0300

    Created the Poll class

---
+++ Poll.py
@@ -0,0 +1,8 @@
+class Poll ():
+
+    def __init__(self,pollName):
+        self.pollName=pollName
+        self.questions=[]
+
+    def addQuestion(self,question):
+        self.questions.append(question)
