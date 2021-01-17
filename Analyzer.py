b1bc4c93f1ee45a964fa3398b9a01c23993af682
sulovadordali <56430166+sulovadordali@users.noreply.github.com>
2021-01-17 14:22:08 +0300

    Analyzer.py has created.

---
+++ Analyzer.py
@@ -0,0 +1,110 @@
+from PollOutput import *
+from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.backends.backend_pdf
+
+
+class Analyzer:
+
+    def __init__(self, students, pollReports):
+        self.students = students
+        self.pollReports = pollReports
+        self.pollOutput = PollOutput()
+        self.attendance()
+        self.calculateSuccessRate()
+        self.statisticsGraph()
+
+    def attendance(self):
+        attendance = dict((i, []) for i in self.students)
+        date = []
+        dateOfPoll = " "
+        condition = 0
+        for i in range(0, len(self.pollReports)):
+            attendancePoll = 0
+            j = self.pollReports[i]
+            if i == 0:
+                dateOfPoll = j.date
+            if i + 1 < len(self.pollReports):
+                if not dateOfPoll == self.pollReports[i + 1].date:
+                    dateOfPoll = self.pollReports[i + 1].date
+                    condition = 1
+            else:
+                condition = 1
+            if not j.date in date:
+                date.append(j.date)
+            for x in j.questionsAndGivenAnswers:
+                if x.student in self.students:
+                    if not j.date in attendance[x.student]:
+                        attendance[x.student].append(j.date)
+            if condition == 1:
+                for stu in attendance.keys():
+                    for dates in attendance[stu]:
+                        if dates == j.date:
+                            attendancePoll += 1
+                pdfName = "./outputs/attendanceCharts/" + str(j.date) + "_attendance.pdf"
+                with matplotlib.backends.backend_pdf.PdfPages(pdfName) as export_pdf:
+                    attAnsAndPer = {}
+                    attAnsAndPer["Absent " + str(len(self.students) - attendancePoll)] = len(
+                        self.students) - attendancePoll
+                    attAnsAndPer["Attended " + str(attendancePoll)] = attendancePoll
+                    plt.pie(attAnsAndPer.values(), labels=attAnsAndPer.keys(), autopct='%.1f%%',
+                            wedgeprops={'linewidth': 1, 'linestyle': 'solid', 'antialiased': True})
+                    export_pdf.savefig()
+                    plt.close()
+                condition = 0
+        attendanceStat = {}
+        for i in attendance.keys():
+            attendanceStat[i] = (len(date), len(attendance[i]), len(attendance[i]) / len(date) * 100)
+
+        self.pollOutput.creatingAttendanceFile(attendance, date)
+
+    def calculateSuccessRate(self):
+        s_total_list = dict((i, []) for i in self.students)
+        for pr in self.pollReports:
+            s_list = dict((i, []) for i in self.students)
+            if pr.poll.pollName != 'Attendance Poll':
+                for s in self.students:
+                    total_right = 0
+                    for r in pr.pollReportRows:
+                        if r.student == s:
+                            for q in r.questionsAndGivenAnswers:
+                                if q.isCorrect:
+                                    s_list[s].append(1)
+                                    total_right += 1
+                                else:
+                                    s_list[s].append(0)
+                    s_total_list[s].append(pr.poll.pollName + " " + pr.date)
+                    s_total_list[s].append(((total_right / len(pr.poll.questions)) * 10000) / 100)
+                    s_list[s].append(str(total_right) + "/" + str(len(pr.poll.questions)))
+                    s_list[s].append(((total_right / len(pr.poll.questions)) * 10000) / 100)
+
+                outputName = pr.date + " " + pr.poll.pollName
+                self.pollOutput.creatCalculateSuccessRate(outputName, len(pr.poll.questions), s_list)
+        self.pollOutput.outputSum(s_total_list)
+
+    def statisticsGraph(self):
+        for pr in self.pollReports:
+            if not "Attendance" in pr.poll.pollName:
+                pdfName = "./outputs/charts/" + str(pr.date) + " " + str(pr.poll.pollName) + ".pdf"
+                with matplotlib.backends.backend_pdf.PdfPages(pdfName) as export_pdf:
+                    for q in pr.questionsInPollReport:
+                        ansAndPer = {}
+                        colors = []
+                        red = 'red'
+                        green = 'lime'
+                        total = 0
+                        for i in q.answers.values():
+                            total += i
+                        for ans in q.answers.keys():
+                            if ans in q.question.correctAnswer:
+                                colors.append(green)
+                            else:
+                                colors.append(red)
+                            ansAndPer[ans] = int((q.answers[ans] / total) * 10000) / 100
+                        fig = plt.figure(figsize=(15, 10))
+                        plt.title(q.question.questionText, fontsize=10)
+                        plt.pie(ansAndPer.values(), labels=ansAndPer.keys(), colors=colors, autopct='%.1f%%',
+                                wedgeprops={'linewidth': 1, 'edgecolor': 'black', 'linestyle': 'solid',
+                                            'antialiased': True})
+                        export_pdf.savefig()
+                        plt.close()
--- PollAnalyzer.py
+++ PollAnalyzer.py
@@ -3,17 +3,15 @@ from Config import *
 from Student import *
 from Poll import *
 from PollReport import *
-from PollOutput import *
+from Analyzer import *
 from os import listdir
 from os.path import isfile, join
 from QuestionAndGivenAnswer import *
-from matplotlib import pyplot as plt
-import numpy as np
 import xlrd as xlrd
 import csv
 import datetime as dt
 import re
-import matplotlib.backends.backend_pdf
+
 
 class PollAnalyzer:
     def __init__(self):
@@ -22,16 +20,13 @@ class PollAnalyzer:
         self.polls = []  # All polls in the system.
         self.pollReports = []  # All poll reports in the system.
         self.config = Config()  # Stores paths.
-        self.pollOutput=PollOutput()
 
     def startAnalyzer(self):
         self.readStudent()
         self.readAnswerKeys()
         self.readPollReports()
         self.updatePollReports()
-        self.calculateSuccessRate()
-        self.attendance()
-        self.statisticsGraph()
+        analyzer = Analyzer(self.students, self.pollReports)
 
     def readStudent(self):  # Reads all students in student list which stored in config.studentListDirectory path
         f = xlrd.open_workbook(self.config.studentListDirectory)
@@ -82,12 +77,12 @@ class PollAnalyzer:
             # Read rows one by one.
             with open(path, 'r', encoding="utf-8") as file:
                 reader = csv.DictReader(file)
-                pollKey = 1     # Count of polls in that poll reports file.
+                pollKey = 1  # Count of polls in that poll reports file.
                 i = 0
                 prev = " "
-                questionList = dict()   # Stores all questions in this poll report.
-                answerList = dict()     # Stores all answers in this poll report.
-                questionsAndGivenAnswersList = dict()   # Stores all QuestionAndGivenAnswer objects in this poll report.
+                questionList = dict()  # Stores all questions in this poll report.
+                answerList = dict()  # Stores all answers in this poll report.
+                questionsAndGivenAnswersList = dict()  # Stores all QuestionAndGivenAnswer objects in this poll report.
                 date = " "
                 # Read rows in that poll report file one by one.
                 date, pollKey = self.readPollReportRow(answerList, date, i, pollKey, prev, questionList,
@@ -100,7 +95,7 @@ class PollAnalyzer:
 
     def readPollReportRow(self, answerList, date, i, pollKey, prev, questionList, questionsAndGivenAnswersList, reader):
         for row in reader:
-            date = row["Submitted Date/Time"]   # Find date of that poll report.
+            date = row["Submitted Date/Time"]  # Find date of that poll report.
             questionKey = 1
 
             # Check that is there more than one poll in that poll report file.
@@ -114,7 +109,7 @@ class PollAnalyzer:
 
             # Iterate questions and given answers to these questions.
             for q in row[None]:
-                q = self.removeSpecialCharacters(q)     # Remove escape characters in questionText.
+                q = self.removeSpecialCharacters(q)  # Remove escape characters in questionText.
                 if j % 2 == 0:
                     # Add questionText into questionList.
                     questionList[str(pollKey) + "." + str(questionKey)] = q
@@ -140,96 +135,6 @@ class PollAnalyzer:
                     questionKey += 1
                 j += 1
         return date, pollKey
-
-    def attendance(self):
-        attendance = dict((i, []) for i in self.students)
-        date = []
-        dateOfPoll = " "
-        condition = 0
-        for i in range (0,len(self.pollReports)):
-            attendancePoll = 0
-            j = self.pollReports[i]
-            if i == 0:
-                dateOfPoll = j.date
-            if i+1 < len(self.pollReports):
-                if not dateOfPoll == self.pollReports[i+1].date:
-                    dateOfPoll = self.pollReports[i+1].date
-                    condition = 1
-            else :
-                condition = 1
-            if not j.date in date:
-                date.append(j.date)
-            for x in j.questionsAndGivenAnswers:
-                if x.student in self.students:
-                    if not j.date in attendance[x.student]:
-                        attendance[x.student].append(j.date)
-            if condition == 1:
-                for stu in attendance.keys():
-                    for dates in attendance[stu]:
-                        if dates == j.date:
-                            attendancePoll += 1
-                pdfName = "./outputs/attendanceCharts/"+str(j.date)+"_attendance.pdf"
-                with matplotlib.backends.backend_pdf.PdfPages(pdfName) as export_pdf:
-                    attAnsAndPer = {}
-                    attAnsAndPer["Absent " + str(len(self.students) - attendancePoll) ] =  len(self.students) - attendancePoll
-                    attAnsAndPer["Attended " + str(attendancePoll)] = attendancePoll
-                    plt.pie(attAnsAndPer.values(), labels=attAnsAndPer.keys(), autopct='%.1f%%',wedgeprops={'linewidth': 1, 'linestyle': 'solid', 'antialiased': True})
-                    export_pdf.savefig()
-                    plt.close()
-                condition = 0
-        attendanceStat = {}
-        for i in attendance.keys():
-            attendanceStat[i] = (len(date), len(attendance[i]), len(attendance[i]) / len(date) * 100)
-
-        self.pollOutput.creatingAttendanceFile(attendance,date)
-    def calculateSuccessRate(self):
-        s_total_list = dict((i, []) for i in self.students)
-        for pr in self.pollReports:
-            s_list = dict((i, []) for i in self.students)
-            if pr.poll.pollName != 'Attendance Poll':
-                for s in self.students:
-                    total_right = 0
-                    for r in pr.pollReportRows:
-                        if r.student == s:
-                            for q in r.questionsAndGivenAnswers:
-                                if q.isCorrect:
-                                    s_list[s].append(1)
-                                    total_right += 1
-                                else:
-                                    s_list[s].append(0)
-                    s_total_list[s].append(pr.poll.pollName + " " + pr.date)
-                    s_total_list[s].append(((total_right / len(pr.poll.questions)) * 10000) / 100)
-                    s_list[s].append(str(total_right) + "/" + str(len(pr.poll.questions)))
-                    s_list[s].append(((total_right / len(pr.poll.questions)) * 10000) / 100)
-
-                outputName = pr.date + " " + pr.poll.pollName
-                self.pollOutput.creatCalculateSuccessRate(outputName,len(pr.poll.questions),s_list)
-        self.pollOutput.outputSum(s_total_list)
-
-    def statisticsGraph(self):
-        for pr in self.pollReports:
-            if not "Attendance" in pr.poll.pollName:
-                pdfName = "./outputs/charts/" + str(pr.date) + " " + str(pr.poll.pollName) + ".pdf"
-                with matplotlib.backends.backend_pdf.PdfPages(pdfName) as export_pdf:
-                    for q in pr.questionsInPollReport:
-                        ansAndPer = {}
-                        colors = []
-                        red = 'red'
-                        green = 'lime'
-                        total = 0
-                        for i in q.answers.values():
-                            total += i
-                        for ans in q.answers.keys():
-                            if ans in q.question.correctAnswer:
-                                colors.append(green)
-                            else:
-                                colors.append(red)
-                            ansAndPer[ans] = int((q.answers[ans] / total) * 10000) / 100
-                        fig = plt.figure(figsize=(15, 10))
-                        plt.title(q.question.questionText,fontsize = 10)
-                        plt.pie(ansAndPer.values(), labels=ansAndPer.keys(), colors=colors, autopct='%.1f%%',wedgeprops={'linewidth': 1,'edgecolor':'black', 'linestyle': 'solid', 'antialiased': True})
-                        export_pdf.savefig()
-                        plt.close()
 
     # Finds identities of polls' in poll report file.
     def findPoll(self, pollKey, questionList):
@@ -352,4 +257,3 @@ class PollAnalyzer:
     def removeSpecialCharacters(self, cellValue):
         text = re.sub(r"[\r\n\t\x07\x0b]", "", cellValue)
         return text
-
